06f82525f5dea98aa6449bc34c3461ed26c249ee
anthonyymc <31148357+anthonyymc@users.noreply.github.com>
2017-08-19 19:46:18 +0800

    Version_1.1

--- smartmatch.py
+++ smartmatch.py
@@ -1,5 +1,6 @@
 import io
 import os
+import sys
 
 # Imports the Google Cloud client library
 from google.cloud import vision
@@ -12,7 +13,7 @@ d.clear()
 file3 = open('/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/order_records.csv', 'r')
 for line2 in file3:
     file2 = open(''.join(["/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/", line2.split(',')[1].rstrip()]), 'r')
-    d['orderid']=line2.split(',')[1].rstrip()
+    d['orderid']=line2.split(',')[0].rstrip()
     d['factory']=line2.split(',')[2].rstrip()
     for line in file2:
         d[line.split(',')[0].rstrip()]=line.split(',')[1].rstrip()
@@ -24,6 +25,30 @@ for line2 in file3:
     
 file3.close()
 
+input_weight={}
+
+weight_file = open('/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/scoring_weight.csv', 'r')
+
+for weight in weight_file:
+    input_weight[weight.split(',')[0].rstrip()]=weight.split(',')[1].rstrip()
+    
+weight_file.close()
+
+fac_dict={}
+fac_list=[]
+
+fac_file = open('/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/factory.csv', 'r')
+for factory in fac_file:
+    fac_dict['facid']=factory.split(',')[0].rstrip()
+    fac_dict['capacity']=factory.split(',')[1].rstrip()
+    fac_dict['performance']=factory.split(',')[2].rstrip()
+    fac_dict['certified']=factory.split(',')[3].rstrip()
+    
+    fac_list.append(fac_dict.copy())
+    fac_dict.clear()
+    
+fac_file.close()
+
 #print(len(dlist))
 
 # Instantiates a client
@@ -32,7 +57,7 @@ client = vision.ImageAnnotatorClient()
 # The name of the image file to annotate
 file_name = os.path.join(
     os.path.dirname(__file__),
-    'shirt02.jpg')
+    str(sys.argv[1]))
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
@@ -43,12 +68,12 @@ image = types.Image(content=content)
 response = client.label_detection(image=image)
 labels = response.label_annotations
 
-os.remove("/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/input_image.csv")
+os.remove(''.join(["/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/", str(sys.argv[1]).split('.')[0], ".csv"]))
 
 d2={}
 d2.clear()
 
-file = open('/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/input_image.csv','a')
+file = open(''.join(["/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/", str(sys.argv[1]).split('.')[0], ".csv"]),'a')
 
 for label in labels:
     file.write(''.join([label.description, ",", str(round(label.score,9)), "\n"])) 
@@ -56,22 +81,94 @@ for label in labels:
     
 file.close()
 
-print (dlist[0])
+fac_result_dict={}
+temp_fac_dict={}
+fac_result_list=[]
 
-#for order in range(0,len(dlist)):
-#	score=0
-#	if list(d2.keys())[order] in dlist[order]
-#	    print(list(d2.keys())[order])
-#	    print("OK")
- #   else
-  #      print(list(d2.keys())[order])
-   #     print("NOT OK")
+for fac_item in range(0,len(fac_list)):
+	fac_result_dict.clear()
+	fac_score=0
+	
+	temp_fac_dict = fac_list[fac_item]
+	
+	fac_result_dict['facid']=temp_fac_dict['facid']
 
-#print(list(d2.keys())[0])
-#print(list(d2.values())[0])
+	for fac_element in range(1,len(fac_list[fac_item])):
+		if list(temp_fac_dict.keys())[fac_element] in input_weight:
+			fac_score += round(float(temp_fac_dict[list(temp_fac_dict.keys())[fac_element]])*float(input_weight[list(temp_fac_dict.keys())[fac_element]]),2)
+			fac_result_dict[list(temp_fac_dict.keys())[fac_element]]=str(round(float(temp_fac_dict[list(temp_fac_dict.keys())[fac_element]])*float(input_weight[list(temp_fac_dict.keys())[fac_element]]),2))
+		else:
+			fac_score += round(float(temp_fac_dict[list(temp_fac_dict.keys())[fac_element]]),2)
+			fac_result_dict[list(temp_fac_dict.keys())[fac_element]]=str(round(float(temp_fac_dict[list(temp_fac_dict.keys())[fac_element]]),2))
+	
+	fac_result_dict['score']=str(round(fac_score,2))
+	fac_result_list.append(fac_result_dict.copy())
 
-#if 'key1' in dict:
-#  print "blah"
-#else:
-#  print "boo"
+sorted_fac_result_list = sorted(fac_result_list, key=lambda d: d['score'], reverse=True)
+
+os.remove("/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/factory_evaluation.csv")
+
+result_file = open('/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/factory_evaluation.csv','a')
+
+for result in range(0,len(sorted_fac_result_list)):
+    result_file.write(''.join([sorted_fac_result_list[result]["facid"], ",", sorted_fac_result_list[result]["score"], "\n"])) 
+    
+result_file.close()
+
+os.remove("/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/fac_score_details.csv")
+
+score_file = open('/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/fac_score_details.csv','a')
+
+for score_result in range(0,len(sorted_fac_result_list)):
+	score_file.write(str(sorted_fac_result_list[score_result]).strip("{").strip("}"))
+	score_file.write("\n")
+    
+score_file.close()
+
+result_dict={}
+result_list=[]
+
+for order in range(0,len(dlist)):
+	result_dict.clear()
+	score=0
+	result_dict['orderid']=dlist[order]["orderid"]
+	result_dict['factory']=dlist[order]["factory"]
+	
+	for element in range(0,len(d2)):
+		if list(d2.keys())[element] in dlist[order]:
+			if list(d2.keys())[element] in input_weight:
+				score += round((1-abs(float(dlist[order][list(d2.keys())[element]]) - float(list(d2.values())[element])))*float(input_weight[list(d2.keys())[element]]),2)
+				result_dict[list(d2.keys())[element]]=str(round((1-abs(float(dlist[order][list(d2.keys())[element]]) - float(list(d2.values())[element])))*float(input_weight[list(d2.keys())[element]]),2))
+			else:
+				score += round(1-abs(float(dlist[order][list(d2.keys())[element]]) - float(list(d2.values())[element])),2)
+				result_dict[list(d2.keys())[element]]=str(round(1-abs(float(dlist[order][list(d2.keys())[element]]) - float(list(d2.values())[element])),2))
+		else:
+			result_dict[list(d2.keys())[element]]=0
+	
+
+	result_dict['score']=str(round(score,2))
+	result_list.append(result_dict.copy())
+	
+sorted_result_list = sorted(result_list, key=lambda d: d['score'], reverse=True)
+
+os.remove("/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/factory_ranking.csv")
+
+result_file = open('/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/factory_ranking.csv','a')
+
+for result in range(0,len(sorted_result_list)):
+    result_file.write(''.join([sorted_result_list[result]["orderid"], ",", sorted_result_list[result]["factory"], ",", sorted_result_list[result]["score"], "\n"])) 
+    
+result_file.close()
+
+os.remove("/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/order_score_details.csv")
+
+score_file = open('/Users/LESTERMOK/Documents/Virtualenvs/smartmatch/smartmatch/order_score_details.csv','a')
+
+for score_result in range(0,len(sorted_result_list)):
+	score_file.write(str(sorted_result_list[score_result]).strip("{").strip("}"))
+	score_file.write("\n")
+    
+score_file.close()
+
+
 
